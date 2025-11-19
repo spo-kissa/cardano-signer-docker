0d6cbbceea090e0a9b906a41f154b6036bcff254
Ii Daisuke <dai@e-frontier.systems>
2025-11-19 13:08:29 +0900

    Linux用起動シェルスクリプトを追加

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+echo Dockerfileをビルドします...
+docker build -t signer:1.0 .
+
+echo Dockerを起動します...
+docker run --rm -dit --init -v ./share:/root/share --name spokissa-signer signer:1.0
+
+echo SIP-8署名を実行します...
+docker exec -it spokissa-signer bash sign.sh
+
+echo Dockerを停止します...
+docker stop spokissa-signer
+
+echo DockerImageを削除します...
+docker image rm signer:1.0
+
